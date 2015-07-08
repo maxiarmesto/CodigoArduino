927bfda421b1ca011db1d377e9563670536dfcb9
maxiarmesto <maxifcefyn@gmail.com>
2015-07-08 17:16:22 -0300

    Codigo con funciones para arduino

---
+++ Arduino.ino
@@ -0,0 +1,99 @@
+#include <TimerOne.h>
+#include <Wire.h>
+//codigo prueba
+const int MPU=0x68;
+
+const int PWM1 = 9;
+const int PWM2 = 10;
+const int INTE0 = 2;
+const int INTE1 = 3;
+
+
+
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int encoder1 = 0;       //contadores encoders ruedas 
+int encoder2 = 0; 
+
+void init_MPU6050();
+
+
+void setup() 
+{
+  init_MPU6050();
+  TCCR2B = TCCR2B & 0b11111000 | 0x01;   // Establece frecuencia pwm pines 9,10 a 31K
+  Timer1.initialize(250000);            // Dispara cada 100 ms
+  Timer1.attachInterrupt(ISR_Timer);    // Activa la interrupcion y la asocia a ISR_Blink
+  Serial.begin(9600);                  // Inicio la transmision serie
+  analogWrite(PWM1,127);               // PWM1 50% duty 
+  analogWrite(PWM2,127);               // PWM2 50% duty
+  attachInterrupt(0, ISR_INTE0, RISING); // Interrupcion externa en pin 2 por flanco de subida
+  attachInterrupt(1, ISR_INTE1, RISING); // Interrupción externa en pin 3 por flanco de subida
+ Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+}
+
+void loop() 
+{
+  
+}
+
+void init_MPU6050()
+{
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);  // PWR_MGMT_1 registro a cero
+  Wire.write(0);     // se saca el MPU del sleep
+  Wire.endTransmission(true);
+}
+
+void ISR_Timer()
+{
+  Serial.print("j");
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+}
+
+void ISR_INTE0()
+{
+  encoder1++;
+  Serial.print("INTE0");
+}
+
+void ISR_INTE1()
+{
+  encoder2++;
+  Serial.print("INTE1");
+}
+
---
+++ TransferenciaDatos.ino
@@ -0,0 +1,125 @@
+void send(char*);
+void receive();
+char formlist[100];
+char data_rec[100];
+int respuestaid        = 0;
+int MICRO_ADDR         = 10; //Verificar
+int flag_uart          = 0;
+char SerRx;
+int data_len_rx        = 0;
+int flag_accion        = 0;
+
+void setup() {
+  
+    Serial.begin(9600);
+}
+
+void loop() {
+
+    if (Serial.available() > 0){
+        receive();
+    }
+    
+}
+
+void send(char* data_send)
+{
+    int data_len     = 0;
+    char caracter    = 0;
+    int header       = 0;
+    int iheader      = 0;
+    int PL           = 0;
+    int mod_header   = 0;
+    int mod_iheader  = 0;
+    int size_l       = 0;
+    int size_h       = 0;
+
+    header   = 160;
+    iheader  = 64;
+
+    while(caracter!='!')
+    {
+        caracter = data_send[data_len];
+        data_len++;
+    }    
+    data_len--;
+
+    PL          = 16;
+    mod_header  = header  + PL + ((data_len & 0xf0000)>>16);
+    mod_iheader = iheader;
+    size_l      = data_len & 0xff;
+    size_h      = (data_len & 0xff00)>>8;
+    formlist[0] = mod_header;
+    formlist[1] = size_h;
+    formlist[2] = size_l;
+    formlist[3] = respuestaid;
+    formlist[4] = MICRO_ADDR;
+
+    for( int i = 5 ; i < 5 + data_len ; i++)
+    {
+        formlist[i] = data_send[i-5];
+    }
+    formlist[5+data_len] = mod_iheader;
+
+    for(int i = 0 ; i < data_len + 6; i++)
+    {
+        Serial.println(formlist[i]);
+        delay(1);
+    }
+
+}
+
+void receive(){
+  
+     SerRx = Serial.read();
+     //Serial.println(SerRx);
+     if( ( flag_uart == 0 ) && ( (SerRx&0xE0) == 160) )//Encontramos el MIT
+     {
+        flag_uart++;
+        data_len_rx = (SerRx & 0x0F);
+        //Serial.println(SerRx);
+     }
+     else if ( flag_uart == 1 )
+     {
+         data_len_rx = data_len_rx + (SerRx<<8);
+         flag_uart++;
+         //Serial.println(SerRx);
+     }
+     else if ( flag_uart == 2 )
+     {
+         data_len_rx = data_len_rx + SerRx;
+         flag_uart++;
+         //Serial.println(SerRx);
+
+     }
+     else if( ( SerRx == MICRO_ADDR )&& ( flag_uart == 3 ) )
+     {
+        flag_uart++;
+        //Serial.println(SerRx);
+     }
+     else if( flag_uart == 4 )
+     {
+        respuestaid = SerRx;
+        flag_uart++;
+        //Serial.println(SerRx);
+     }
+     else if ( (flag_uart > 4) && (flag_uart < 5 + data_len_rx) )
+     {
+         data_rec[flag_uart-5]=SerRx;
+         flag_uart++;
+         //Serial.println(SerRx);
+     }
+     else if( (SerRx == 64) && ( flag_uart == (5 + data_len_rx) ) )//termina la recepción de la trama
+     {
+        flag_uart = 0;
+        //for(i = 0; i < data_len_rx; i++)
+        //{
+        //    Serial.println(data_rec[i]);    //Mandamos los datos que hemos recibido
+        //}
+       flag_accion = 1;   //Ejecutamos la accion en el loop principal
+       
+     }
+     else flag_uart = 0;
+
+ }
+
